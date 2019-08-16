d482d69b95df52690af392c5405067e238435489
unknown <Alistair>
2019-08-15 23:39:07 -0400

    Initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,80 @@
+let cpuOptions = ["r", "p", "s"],
+
+    wins = 0,
+    losses = 0,
+    ties = 0,
+
+    userChoiceText = document.getElementById("user-choice"),
+    cpuChoiceText = document.getElementById("cpu-choice"),
+    winsText = document.getElementById("wins"),
+    lossesText = document.getElementById("losses"),
+    tiesText = document.getElementById("ties");
+
+document.onkeyup = function(event) {
+
+  let cpuGuess = cpuOptions[Math.floor(Math.random() * cpuOptions.length)],
+      userGuess = event.key;
+
+  if ((userGuess === "r" || "p" || "s")) {
+
+    if ((userGuess === "r" && cpuGuess === "s") ||
+      (userGuess === "s" && cpuGuess === "p") || 
+      (userGuess === "p" && cpuGuess === "r")) {
+      wins++;
+
+    } else if (userGuess === cpuGuess) {
+      ties++;
+
+    } else {
+      losses++;
+    }
+
+    userChoiceText.innerHTML = `<p>You chose: ${userGuess}</p>`;
+    cpuChoiceText.innerHTML = `<p>The computer chose: ${cpuGuess}</p>`;
+    winsText.innerHTML = `<p>Wins: ${wins}</p>`;
+    lossesText.innerHTML = `<p>Losses: ${losses}</p>`;
+    tiesText.innerHTML = `<p>Ties: ${ties}</p>`;
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
